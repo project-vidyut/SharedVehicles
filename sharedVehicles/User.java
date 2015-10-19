47811333f7a803f95350b6e9318e982a990be444
Mahesha Hiremath <mahesha@projectvidyut.org>
2015-10-19 15:53:41 +0530

    Skeleton of User Class extending Movable created.
    Empty methods....

---
+++ sharedVehicles/User.java
@@ -0,0 +1,30 @@
+package sharedVehicles;
+
+public class User extends Movable {
+
+	public User(int xdir, int ydir) {
+		super(xdir, ydir);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void pickUpVehicle() {
+		
+	}
+	
+	public void dropVehicle() {
+		
+	}
+	
+	public void getIntoMetro() {
+		
+	}
+	
+	public void getDownFromMetro() {
+		
+	}
+	
+	public int payMoney() {
+		return 0;
+	}
+
+}
