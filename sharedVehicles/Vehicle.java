a6942f96bb3abeedd6029f4a0fee193f864a7c39
Mahesha Hiremath <mahesha@projectvidyut.org>
2015-10-19 15:55:08 +0530

    Skeleton of Vehicle clas extending Movable created

---
+++ sharedVehicles/Vehicle.java
@@ -0,0 +1,39 @@
+package sharedVehicles;
+
+public class Vehicle extends Movable {
+
+	public Vehicle(int xdir, int ydir) {
+		super(xdir, ydir);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getCharge() {
+		return 0;
+	}
+	
+	public void setCharge() {
+		
+	}
+	
+	public void startCharging() {
+		
+	}
+	
+	public void endCharging() {
+		
+	}
+	
+	public void startMeter() {
+		
+	}
+	
+	public void stopMeter() {
+		
+	}
+	
+	public void getCost() {
+		
+	}
+
+	
+}
