2d26ba4c13d6daf4173e3d8a28867645600663f4
Mahesha Hiremath <mahesha@projectvidyut.org>
2015-10-29 14:26:23 +0530

    Adding to and fro motion method

--- sharedVehicles/Movable.java
+++ sharedVehicles/Movable.java
@@ -12,44 +12,31 @@ public int xdir;  // -1, 0, or 1
 public int ydir;  // -1, 0, or 1
 public int pathIndex = 0;
 int[][] Path = new int[8][2];
+public int stopCount = 0;
+public int stopCount1 = 0;
 
 
 
-public Movable(int xdir, int ydir)
+public Movable(int xdir, int ydir, int[][] p)
     {
     this.xdir = xdir;
     this.ydir = ydir;
+    Path = p;
     }
 
 public void step(SimState state)
     {
+	
+	int stopX = 20,stopY =40, stopX1 = 80, stopY1=60;
+	
+	
+	
+	
     Simulation tut = (Simulation)state;
     
 
     Int2D location = tut.particles.getObjectLocation(this);
-    Path[0][0] = 20;
-    Path[0][1] = 0;
 
-    Path[1][0] = 20;
-    Path[1][1] = 50;
-
-    Path[2][0] = 50;
-    Path[2][1] = 50;
-
-    Path[3][0] = 50;
-    Path[3][1] = 20;
-
-    Path[4][0] = 80;
-    Path[4][1] = 20;
-
-    Path[5][0] = 80;
-    Path[5][1] = 80;
-
-    Path[6][0] = 50;
-    Path[6][1] = 80;
-
-    Path[7][0] = 50;
-    Path[7][1] = 100;
 
 
     // move
@@ -68,11 +55,30 @@ public void step(SimState state)
     	System.out.println("Path index"+pathIndex);
  //   	moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
     }
-    moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
+    //moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
     
+    if( location.x == stopX && location.y == stopY) {
+    	stopCount = stopCount +1;
+    } 
+    
+   if(stopCount == 0 ||stopCount > 20) {
+   moveToAndFro(Path,tut);
+   stopCount=0;
+   }
+ 
+   /*
+   if( location.x == stopX1 && location.y == stopY1) {
+   	stopCount1 = stopCount1 +1;
+   } 
+   
+  if(stopCount == 0 ||stopCount > 20) {
+  moveToAndFro(Path,tut);
+  stopCount=0;
+  }
+    */
+       
   //  moveObject(tut,20,50,50,50);
-
-    }
+  }
 
 public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
 	System.out.println("Moving :-->"+x1+" "+y1+" "+" "+x2+" "+y2);
@@ -110,9 +116,36 @@ public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
     Int2D newloc = new Int2D(newx,newy);
 
     tutorial.particles.setObjectLocation(this,newloc);    
+	
+}
 
+public void moveToAndFro(int[][] p, Simulation tut) {
 	
-	
+	 
+	    Int2D location = tut.particles.getObjectLocation(this);
+	    
+		 if(pathIndex==3) {
+			 Path = reversePath(Path);
+			 pathIndex=0;
+		 }
+
+	 
+	    int x1 = Path[pathIndex][0], y1 = Path[pathIndex][1], x2 = Path[pathIndex+1][0], y2 = Path[pathIndex+1][1];
+	    
+	            
+	    // set my new location
+	   System.out.println(location.x+" "+location.y);
+	    int dist = distance(Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
+	   
+	    int curDistance = distance(location.x,location.y,Path[pathIndex][0],Path[pathIndex][1]);
+	    System.out.println(curDistance);
+	    if(curDistance >= dist) {
+	    	pathIndex = pathIndex + 1;
+	    	System.out.println("Path index"+pathIndex);
+	 //   	moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
+	    }
+	    moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
+	    
 }
 
 public int distance(int x1,int y1, int x2, int y2) {
@@ -124,6 +157,19 @@ public void stop() {
 	
 }
 
+public int[][] reversePath(int[][] p) {
+
+	
+	for(int i = 0; i < p.length / 2; i++)
+	{
+	    int[] temp = p[i];
+	    p[i] = p[p.length - i - 1];
+	    p[p.length - i - 1] = temp;
+	}
+	
+	return p;
+	
+}
 
 
 }
