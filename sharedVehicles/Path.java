b0d1ad7005915913de211f3817c8f215c36a5e9d
Mahesha Hiremath <mahesha@projectvidyut.org>
2015-10-29 14:29:34 +0530

    creating basic "Path" class...

---
+++ sharedVehicles/Path.java
@@ -0,0 +1,12 @@
+package sharedVehicles;
+
+public class Path {
+	public int[][] points;
+	public boolean[] stops;
+	
+	
+	public Path(int n,boolean[] b,int[][] p) {
+	points = p;
+	stops = b;
+	}
+}
