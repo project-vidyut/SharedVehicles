8cc131ecc264f7d3f8ca2ff3bf37ddb71e16b543
Mahesha Hiremath <mahesha@projectvidyut.org>
2015-11-11 03:00:03 +0530

    Step method emptied. stop method created.

--- sharedVehicles/Movable.java
+++ sharedVehicles/Movable.java
@@ -25,59 +25,8 @@ public Movable(int xdir, int ydir, int[][] p)
     }
 
 public void step(SimState state)
-    {
-	
-	int stopX = 20,stopY =40, stopX1 = 80, stopY1=60;
-	
-	
-	
-	
-    Simulation tut = (Simulation)state;
-    
+    { 
 
-    Int2D location = tut.particles.getObjectLocation(this);
-
-
-
-    // move
- 
-    int x1 = Path[pathIndex][0], y1 = Path[pathIndex][1], x2 = Path[pathIndex+1][0], y2 = Path[pathIndex+1][1];
-    
-            
-    // set my new location
-   System.out.println(location.x+" "+location.y);
-    int dist = distance(Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
-   
-    int curDistance = distance(location.x,location.y,Path[pathIndex][0],Path[pathIndex][1]);
-    System.out.println(curDistance);
-    if(curDistance >= dist) {
-    	pathIndex = pathIndex + 1;
-    	System.out.println("Path index"+pathIndex);
- //   	moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
-    }
-    //moveObject(tut,Path[pathIndex][0],Path[pathIndex][1],Path[pathIndex+1][0],Path[pathIndex+1][1]);
-    
-    if( location.x == stopX && location.y == stopY) {
-    	stopCount = stopCount +1;
-    } 
-    
-   if(stopCount == 0 ||stopCount > 20) {
-   moveToAndFro(Path,tut);
-   stopCount=0;
-   }
- 
-   /*
-   if( location.x == stopX1 && location.y == stopY1) {
-   	stopCount1 = stopCount1 +1;
-   } 
-   
-  if(stopCount == 0 ||stopCount > 20) {
-  moveToAndFro(Path,tut);
-  stopCount=0;
-  }
-    */
-       
-  //  moveObject(tut,20,50,50,50);
   }
 
 public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
@@ -86,7 +35,7 @@ public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
     int newx=location.x,newy=location.y;
     
     if(location.x == x2) {
-    	System.out.println("Entering x=x2");
+  //  	System.out.println("Entering x=x2");
     	if(y2 > y1) {
         	xdir=0;
         	ydir=1;
@@ -95,10 +44,10 @@ public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
         	xdir=0;
         	ydir=-1;
     	}
-    	System.out.println("xdir:"+xdir+" "+"ydir"+ydir);
+ //   	System.out.println("xdir:"+xdir+" "+"ydir"+ydir);
     }
     if(location.y == y2) {
-    	System.out.println("Entering y=y2");	
+  //  	System.out.println("Entering y=y2");	
     	if(x2 > x1) {
         	xdir=1;
         	ydir=0;
@@ -107,7 +56,7 @@ public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
         	xdir=-1;
         	ydir=0;
     	}
-    	System.out.println("xdir:"+xdir+" "+"ydir"+ydir);	
+  //  	System.out.println("xdir:"+xdir+" "+"ydir"+ydir);	
     }
  
             
@@ -121,10 +70,11 @@ public void moveObject(Simulation tutorial,int x1,int y1,int x2, int y2) {
 
 public void moveToAndFro(int[][] p, Simulation tut) {
 	
+	    int length = p.length-1;
 	 
 	    Int2D location = tut.particles.getObjectLocation(this);
 	    
-		 if(pathIndex==3) {
+		 if(pathIndex==length) {
 			 Path = reversePath(Path);
 			 pathIndex=0;
 		 }
@@ -150,11 +100,14 @@ public void moveToAndFro(int[][] p, Simulation tut) {
 
 public int distance(int x1,int y1, int x2, int y2) {
 	return Math.abs(x1-x2) + Math.abs(y2-y1);
-	
 }
 
-public void stop() {
-	
+public void stop(Simulation tut) {
+    Int2D location = tut.particles.getObjectLocation(this);
+    int newy = location.y;
+    int newx = location.x;
+    Int2D newloc = new Int2D(newx,newy);
+    tut.particles.setObjectLocation(this,newloc);   	
 }
 
 public int[][] reversePath(int[][] p) {
